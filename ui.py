d7b52f6f1ba845f1c620c0b57f3847d31f74fb73
Jakub Matuszczak <kubamatuszczak2@gmail.com>
2024-07-10 00:23:02 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -25,18 +25,34 @@ class MyApp(App):
 
         self.folder_name_input_field = Input(name="folder_name", id="folder_name")
 
+
+
         yield Horizontal(
             Label("Podaj nazwę folderu"),
             self.folder_name_input_field,
             Button("Wprowadź", name="submit_folder_name", id="submit_folder_name"),
             classes="entry"
         )
+
+
+        self.collection_name_input_field = Input(name="col_name", id="col_name")
+
+        yield Horizontal(
+            Label("Podaj nazwę kolekcji"),
+            self.collection_name_input_field,
+            Button("Wprowadź", name="submit_col_name", id="submit_col_name"),
+            classes='entry'
+
+        )
+
         self.label = Label("Wprowadź adres kontraktu")
         self.label2 = Label("Wprowadź klucz prywatny")
         self.label3 = Label("Wprowadź nazwę folderu")
+        self.label4 = Label("Wprowadź nazwę kolekcji")
         yield self.label
         yield self.label2
         yield self.label3
+        yield self.label4
 
     def on_mount(self):
         self.title = "Mintowanie wielu NFT naraz"
@@ -56,7 +72,10 @@ class MyApp(App):
             self.folder_name = self.folder_name_input_field.value
             self.folder_name_input_field.disabled = True
             self.label3 = Label(f"Folder: {self.folder_name}")
-
+        if event.button.id == 'submit_col_name':
+            self.col_name = self.collection_name_input_field.value
+            self.collection_name_input_field.disabled = True
+            self.label4.update(f"Nazwa kolekcji: {self.col_name}")
 
 
         
